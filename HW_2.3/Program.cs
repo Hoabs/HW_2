177c4fa4a855e8a23bbe4c0349abd5a9f46dfcaf
Arsen <hoabs@mail.ru>
2022-12-07 16:30:22 +0300

    Completed HW_2.3

---
+++ HW_2.3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a day of the week count number");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
