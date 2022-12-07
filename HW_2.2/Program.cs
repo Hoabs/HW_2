9994be801d04bfbec8209424317a31ad08de3283
Arsen <hoabs@mail.ru>
2022-12-07 15:44:33 +0300

    Completed HW_2.2

---
+++ HW_2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter any integer number");
+int num = int.Parse(Console.ReadLine());
+int numCorrect = num;
+
+if (numCorrect < 0)
+{
+    numCorrect = -numCorrect;
+}
+
+string numStr = numCorrect.ToString();
+int numLen = numStr.Length;
+
+if (numLen >= 3)
+{
+    int resTemp = numCorrect % (Convert.ToInt32(Math.Pow(10,numLen - 2)));
+    int res = resTemp / (Convert.ToInt32(Math.Pow(10,numLen - 3)));
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Third digit does not exist");
+}
