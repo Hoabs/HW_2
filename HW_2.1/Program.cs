660cc8f0da33acca3bd200f49d72f12428b460af
Arsen <hoabs@mail.ru>
2022-12-07 16:19:25 +0300

    Completed HW_2.1

--- HW_2.1/Program.cs
+++ HW_2.1/Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.WriteLine("Anter a three-digit number");
 int num = Convert.ToInt32(Console.ReadLine());
-int res = (num%100)/10;
+if (num < 0)
+{
+    num = -num;
+}
+int res = (num % 100) / 10;
 Console.WriteLine(res);
